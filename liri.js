545a499cba2d1c2c012338448bd507638c63a691
Michael Vega <ichigo509@yahoo.com>
2019-08-14 22:48:39 -0400

    fixed spotify, bands in town, and omdb, all that is left is cleaning up bands in town and doing the random.txt

--- liri.js
+++ liri.js
@@ -1,27 +1,17 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var Spotify = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var node = process.argv;
 var search = node.slice(3).join(" ")
-var movieName = "";
-for (var i = 2; i < node.length; i++) {
 
-    if (i > 2 && i < node.length) {
-        movieName = movieName + "+" + node[i];
-    } else {
-        movieName += node[i];
-
-    }
-}
-
-var bandsApi = function (api, artist) {
+var apiCall = function (api, artist) {
     if (api === "concert-this") {
         axios
             .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
             .then(function (response) {
-                console.log("Here is the artist's events: " + response.data)
+                console.log(response.data)
             })
             .catch(function (error) {
                 if (error.response) {
@@ -45,19 +35,30 @@ var bandsApi = function (api, artist) {
             })
     } else if (api === "spotify-this-song") {
         spotify
-            .search({ type: "track", query: artist })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (err) {
-                console.log(err);
+            .search({ type: "track", query: artist, limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                //spotify object
+                var spotArr = data.tracks.items
+                //going through the results but limiting it to 1 above
+                for (i = 0; i < spotArr.length; i++) {
+                    console.log(`\nHere are the results: \n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}`)
+                };
             });
     } else if (api === "movie-this") {
         axios
-            .get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy")
+            .get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy")
             .then(
                 function (response) {
-                    console.log(response.data);
+                    console.log("\nResults for: " + JSON.stringify(response.data.Title, null, 2));
+                    console.log("\nYear of release: " + JSON.stringify(response.data.Year, null, 2));
+                    console.log("\nIMDB Rating: " + JSON.stringify(response.data.imdbRating, null, 2));
+                    console.log("\nRotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value, null, 2));
+                    console.log("\nCountries where the movie was produced: " + JSON.stringify(response.data.Country, null, 2));
+                    console.log("\nLanguages in the movie: " + JSON.stringify(response.data.Language, null, 2));
+                    console.log("\nPlot of the movie: " + JSON.stringify(response.data.Plot, null, 2));
+                    console.log("\nActors featured in the movie: " + JSON.stringify(response.data.Actors, null, 2));
                 })
             .catch(function (err) {
                 console.log(err);
@@ -65,4 +66,4 @@ var bandsApi = function (api, artist) {
     }
 }
 
-bandsApi(node[2], search);
+apiCall(node[2], search);
